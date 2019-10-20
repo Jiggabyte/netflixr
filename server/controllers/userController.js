c74a7d51e9436e3c2d86a26b398ac03e0a48e743
jigga.e10@gmail.com <Wulfdog@7>
2019-10-21 00:07:09 +0100

    Express server bug fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,74 +1,87 @@
 const userData = require('../models/userModel');
 
-const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
-//...
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'wulfclan',
+  database: 'netflisa'
+})
+
+connection.connect()
+
 
 // Display list of all Authors.
 exports.signup = function(req, res) {
-    const firstName = req.body.firstN;
-    const lastName = req.body.lastN;
-    const userName = req.body.userN;
-    const email = req.body.email;
-    const pass = req.body.pass;
-    const city = req.body.city;
-    const state = req.body.state;
-    const country = req.body.country;
+  const firstName = req.body.firstN;
+  const lastName = req.body.lastN;
+  const userName = req.body.userN;
+  const email = req.body.email;
+  const pass = req.body.pass;
+  const city = req.body.city;
+  const state = req.body.state;
+  const country = req.body.country;
 
-    MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
-        if (err) throw err;
-        const dbo = db.db("careDB");
-        const myobj = {"firstName": firstName, "lastName": lastName,
-                       "userName": userName, "email": email,
-                       "pass": pass, "city": city,
-                       "state": state, "country": country
+  connection.query('INSERT INTO ', function (err, rows, fields) {
+    if (err) throw err
 
-        };
-        dbo.collection("users").insertOne(myobj, function(err, res) {
-            if (err) throw err;
-            console.log("User Data Inserted");
-            db.close();
-        });
+    console.log('The solution is: ', rows[0].solution)
+  })
+
+  MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+    if (err) throw err;
+    const dbo = db.db("careDB");
+    const myobj = {"firstName": firstName, "lastName": lastName,
+      "userName": userName, "email": email,
+      "pass": pass, "city": city,
+      "state": state, "country": country
+
+    };
+    dbo.collection("users").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("User Data Inserted");
+      db.close();
     });
+  });
 
 
-    res.send(firstName + ' ' + lastName + ' ' + email);
+  res.send(firstName + ' ' + lastName + ' ' + email);
 };
 
 
 
 exports.upload = function(req, res) {
-    const userName = req.body.userN;
-    const email = req.body.email;
+  const userName = req.body.userN;
+  const email = req.body.email;
 
-    if (req.files.user_image.mimetype === 'jpg' || 'jpeg' || 'png' || 'bmp') {
+  if (req.files.user_image.mimetype === 'jpg' || 'jpeg' || 'png' || 'bmp') {
 
-        req.files.user_image.mv('../uploads/' + req.files.user_image.name, function (err) {
+    req.files.user_image.mv('../uploads/' + req.files.user_image.name, function (err) {
 
-            if (err) throw err;
+      if (err) throw err;
 
-            MongoClient.connect(url, {useNewUrlParser: true}, function (err, db) {
-                if (err) throw err;
-                const dbo = db.db("careDB");
-                const myobj = {
-                    "type": "profile", "filename": req.files.user_image.name,
-                    "userName": userName, "email": email
+      MongoClient.connect(url, {useNewUrlParser: true}, function (err, db) {
+        if (err) throw err;
+        const dbo = db.db("careDB");
+        const myobj = {
+          "type": "profile", "filename": req.files.user_image.name,
+          "userName": userName, "email": email
 
-                };
-                dbo.collection("userImager").insertOne(myobj, function (err, res) {
-                    if (err) throw err;
-                    console.log("User Image Inserted");
-                    db.close();
-                });
-            });
-
+        };
+        dbo.collection("userImager").insertOne(myobj, function (err, res) {
+          if (err) throw err;
+          console.log("User Image Inserted");
+          db.close();
         });
+      });
 
-        res.send(userName + ' ' + '' + ' ' + email);
-    }
+    });
+
+    res.send(userName + ' ' + '' + ' ' + email);
+  }
 
 };
 
@@ -77,34 +90,36 @@ exports.upload = function(req, res) {
 
 // Display detail page for a specific Author.
 exports.login = function(req, res) {
-    const userName = req.body.userN;
-    const email = req.body.email;
-    const pass = req.body.pass;
+  const userName = req.body.userN;
+  const email = req.body.email;
+  const pass = req.body.pass;
 
-    res.send(userName + ' ' + ' ' + ' ' + email);
+  res.send(userName + ' ' + ' ' + ' ' + email);
 };
 
 // Display Author create form on GET.
 exports.activate = function(req, res) {
-    res.send('NOT IMPLEMENTED: Activate');
+  res.send('NOT IMPLEMENTED: Activate');
 };
 
 // Handle Author create on POST.
 exports.block = function(req, res) {
-    res.send('NOT IMPLEMENTED: Block');
+  res.send('NOT IMPLEMENTED: Block');
 };
 
 // Display Author delete form on GET.
 exports.report = function(req, res) {
-    res.send('NOT IMPLEMENTED:Report');
+  res.send('NOT IMPLEMENTED:Report');
 };
 
 // Display Author create form on GET.
 exports.delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: Delete');
+  res.send('NOT IMPLEMENTED: Delete');
 };
 
 // Display Author create form on GET.
 exports.edit = function(req, res) {
-    res.send('NOT IMPLEMENTED: Edit');
+  res.send('NOT IMPLEMENTED: Edit');
 };
+
+connection.end()
